415c61ce246404dfda11c35de40cf1dda21ba52c
GabrielVictor0 <bielgvsa@gmail.com>
2023-09-19 10:57:00 -0300

    Domains e interface finalizada, criando repositories e controllers

---
+++ webapi.event+.manha/Controllers/UsuarioController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.event_.manha.Contexts;
+using webapi.event_.manha.Domains;
+using webapi.event_.manha.Interfaces;
+using webapi.event_.manha.Repositories;
+
+namespace webapi.event_.manha.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class UsuarioController : ControllerBase
+    {
+        private IUsuarioRepository _usarioRepositoy;
+
+        public UsuarioController()
+        {
+            _usarioRepositoy = new UsuarioRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Usuario usuario)
+        {
+            try
+            {
+                _usarioRepositoy.Cadastrar(usuario);
+
+                return StatusCode(201);
+            }
+            catch (Exception error)
+            {
+
+                return BadRequest(error.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            try
+            {
+               Usuario usuario = _usarioRepositoy.BuscarPorId(id);
+
+                return Ok(usuario);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
